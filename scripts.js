d426e83c7d98781c02fa025853ec0a62f48d08ab
Sam Fahlberg <sam.fahlberg@gmail.com>
2025-05-16 12:11:01 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,11 +5,14 @@ document.addEventListener('DOMContentLoaded', function () {
     button.addEventListener('click', function () {
       this.classList.toggle('active');
       const panel = this.nextElementSibling;
+      const icon = this.querySelector('.accordion-icon');
 
       if (panel.style.maxHeight) {
         panel.style.maxHeight = null;
+        icon.textContent = '+';
       } else {
         panel.style.maxHeight = panel.scrollHeight + "px";
+        icon.textContent = '−'; // minus sign
       }
     });
   });
