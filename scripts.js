3149b6cb8627205241b68142ecbcc8063e7c633b
Sam Fahlberg <sam.fahlberg@gmail.com>
2025-05-16 12:34:34 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,1 @@
-document.querySelectorAll('.toggle-link').forEach(link => {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    const fullText = this.previousElementSibling;
-    const isVisible = fullText.style.display === 'block';
-    fullText.style.display = isVisible ? 'none' : 'block';
-    this.textContent = isVisible ? this.textContent.replace('–', '+ Read more –') : this.textContent.replace('+ Read more –', '– Show less –');
-  });
-});
+///
